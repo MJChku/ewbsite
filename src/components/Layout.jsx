af39d041686b8ac531e9133b4ec6bf38a32d1728
MJChku <u3533156@hku.hk>
2019-12-09 00:22:58 +0100

    j

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -43,7 +43,7 @@ class Layout extends React.Component{
         <SiteHeader>
           <Link to="/">
             <Logo>MJC</Logo>
-            <Name> {page === "" ? "Ma Jiacheng" : page}</Name>
+            <Name> {page === "" ? "Hello World! My name is Ma Jiacheng" : page}</Name>
           </Link>
           <SiteNav>
             <ul>
